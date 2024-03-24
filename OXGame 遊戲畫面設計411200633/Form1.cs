133adb5952edcda0bc1e879f1cbd50f0fe721253
basuyaanson <basuyaanson@gmail.com>
2024-03-24 22:41:21 +0800

    加入專案檔案。

---
+++ OXGame 遊戲畫面設計411200633/Form1.cs
@@ -0,0 +1,15 @@
+namespace OXGame_遊戲畫面設計411200633
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
